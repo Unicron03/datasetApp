ab4b8eb99ebdd3394e77fc24f5859adf43fa6c06
Enzo Vandepoele <enzo.vandepoele2@gmail.com>
2024-06-06 13:49:57 +0200

    modif visu + essai sys téléchargement expander

--- pages/create.py
+++ pages/create.py
@@ -49,22 +49,30 @@ if st.session_state.df.columns.tolist():
             if col_name != "SIGNATURE":
                 st.session_state.new_row[col_name] = st.text_input(f"Entrez la valeur pour '{col_name}'")
 
-    # Si l'utilisateur clique sur le bouton "Ajouter la ligne"
-    if st.button("Ajouter la ligne"):
-        # Ajouter la nouvelle ligne au DataFrame
-        new_data = pd.DataFrame(st.session_state.new_row, index=[0])
-        st.session_state.df = pd.concat([st.session_state.df, new_data], ignore_index=True)
-        st.session_state.df = move_signature_to_end(st.session_state.df)
+        # Si l'utilisateur clique sur le bouton "Ajouter la ligne"
+        if st.button("Ajouter la ligne"):
+            # Ajouter la nouvelle ligne au DataFrame
+            new_data = pd.DataFrame(st.session_state.new_row, index=[0])
+            st.session_state.df = pd.concat([st.session_state.df, new_data], ignore_index=True)
+            st.session_state.df = move_signature_to_end(st.session_state.df)
 
     # Afficher le DataFrame
+    st.write('### Visualisation du DataFrame :')
     st.dataframe(st.session_state.df)
 
     col1, col2, col3 = st.columns(3)
     with col1:
         # Si l'utilisateur clique sur le bouton "Télécharger", basculer l'état des boutons de téléchargement
-        if st.button("Télécharger"):
-            st.session_state.show_signature_button = False
-            st.session_state.show_download_buttons = not st.session_state.show_download_buttons
+        with st.expander("Télécharger", expanded=True):
+            cols1, cols2, cols3 = st.columns(3)
+            with cols1:
+                st.download_button("Télécharger en CSV", data=st.session_state.df.to_csv(index=False), file_name="data.csv", mime="text/csv")
+            with cols2:
+                st.download_button("Télécharger en JSON", data=st.session_state.df.to_json(orient="records"), file_name="data.json", mime="application/json")
+            with cols3:
+                table = pa.Table.from_pandas(st.session_state.df)
+                pq.write_table(table, 'data.parquet')
+                st.download_button("Télécharger en Parquet", data=open('data.parquet', 'rb').read(), file_name="data.parquet", mime="application/octet-stream")
     with col2:
         if st.button("Ajout signature"):
             st.session_state.show_download_buttons = False
